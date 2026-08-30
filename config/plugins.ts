cb93a96642d47a3b6e4c7a124c168706e5a8b260
Tardu Zaimoglu <tarduzaimoglu@gmail.com>
2026-08-30 14:30:07 +0000

    feat: use local media storage

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,21 +1,12 @@
-export default ({ env }) => ({
+export default () => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-aws-s3',
-      providerOptions: {
-        baseUrl: env('SUPABASE_PUBLIC_URL'),
-        s3Options: {
-          credentials: {
-            accessKeyId: env('SUPABASE_S3_ACCESS_KEY_ID'),
-            secretAccessKey: env('SUPABASE_S3_SECRET_ACCESS_KEY'),
-          },
-          region: env('SUPABASE_S3_REGION'),
-          endpoint: env('SUPABASE_S3_ENDPOINT'),
-          forcePathStyle: true,
-        },
-        params: {
-          Bucket: env('SUPABASE_S3_BUCKET'),
-        },
+      provider: 'local',
+      providerOptions: {},
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
